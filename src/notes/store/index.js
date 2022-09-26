0fd48ec02bb3f6f5c11573988208bd193c7da762
Yasin Türk <85787009+yasin03@users.noreply.github.com>
2022-09-26 16:47:25 +0300

    Delete src directory

--- src/notes/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-// Boş bir merkezi state oluşturuldu
-const StoreContext = createContext();
-
-// Merkezi state dışarıya açıldı
-
-export default StoreContext;
